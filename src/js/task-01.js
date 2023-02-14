6fd0e22a0703374a2c79d4da7cbb1f236d742f37
Filip Vukić <filipvukic90@gmail.com>
2023-02-14 22:02:02 +0100

    initial commit

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const liItems = document.querySelectorAll(".list-item");
+console.log("Number of categories: " + liItems.length);
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(1, 2));
